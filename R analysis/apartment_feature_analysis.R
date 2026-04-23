f9bb8d89644250b686ca53df991f9fd8848d38de
William Johnston <wjohnst1@friars.providence.edu>
2026-04-23 08:35:58 -0400

    Initial analysis for Aparments in R

--- R analysis/apartment_feature_analysis.R
+++ R analysis/apartment_feature_analysis.R
@@ -1,5 +1,11 @@
 library(dplyr)
-apartments <- read.csv("full_rentalDataset_2016_FILTERED.csv")
+apartments <- read.csv("full_rentalDataset_2016_FILTERED_updated.csv")
+
+apartments_filtered <- apartments |> filter(price < 200000 & price > 80) 
+apartments_filtered |> 
+  arrange(price) |> 
+  select(price, street_address) |>
+  head(5)
 
 #               with feature  without feature
 # doorman       21019         28333
@@ -15,6 +21,18 @@ apartments <- read.csv("full_rentalDataset_2016_FILTERED.csv")
 # gym           36074         13278
 # pets allowed  25678         25678
 
-apartments_lm1 <- lm(price ~ bedrooms + bathrooms + bedrooms * bathrooms + pool + furnished + parking.spot + outdoor.space + dishwasher + elevator + in.unit.wash.dryer + in.building.wash.dryer + fitness.center + pet.policy + doorman, data=apartments)
+# basic, ONLY APARTMENT FEATURE lm
+# logged y because dataset has right skew
+apartments_lm <- lm(log(price) ~ bedrooms + bathrooms + pool + furnished + parking.spot + outdoor.space + dishwasher + elevator + in.unit.wash.dryer + in.building.wash.dryer + fitness.center + pet.policy + doorman, data=apartments_filtered)
+summary(apartments_lm)
+
+car::vif(apartments_lm)
+# No multicolinearity problems
+plot(apartments_lm)
+# Residuals vs fitted looks good, no multicolinearity problems I can see
+# Fat tailed distribution still, need to do something about that (log-log the log)
+# Looking into observations 1831, 49284, 47009, 38559, 41570
+apartments |> slice(1831, 49284, 47009, 38559, 41570)
+
 
 
