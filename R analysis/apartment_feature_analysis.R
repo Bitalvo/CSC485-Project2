5de6b895c75d05547589781ecfc73902836f2338
William Johnston <wjohnst1@friars.providence.edu>
2026-04-29 14:05:25 -0400

    Updates

--- R analysis/apartment_feature_analysis.R
+++ R analysis/apartment_feature_analysis.R
@@ -7,7 +7,7 @@ plot(apartments_lm)
 # Residuals vs fitted looks good, no multicolinearity problems I can see
 # Fat tailed distribution still, need to do something about that (log-log the log)
 # Looking into observations 1831, 49284, 47009, 38559, 41570
-apartments |> slice(1831, 49284, 47009, 38559, 41570)
+head(apartments)
 
 apartments_filtered <- apartments |> filter(price <= 35000 & price > 80) 
 
@@ -36,10 +36,11 @@ apartments_filtered <- apartments_filtered |> mutate(bathbeddiff = (bathrooms - 
 # no wash/dry   25186         24166 
 # gym           36074         13278
 # pets allowed  25678         25678
+# 
 
 # basic, ONLY APARTMENT FEATURE lm
 # logged y because dataset has right skew
-apartments_lm <- lm(log(price) ~ bedrooms + bathrooms + pool + parking.spot + outdoor.space + dishwasher + elevator + in.unit.wash.dryer + in.building.wash.dryer + fitness.center + doorman + near_restaurants_1_5_miles + moderate_restaurants_3_miles, data=apartments_filtered)
+apartments_lm <- lm(log(price) ~ bedrooms + bathrooms + pool + parking.spot + outdoor.space + dishwasher + elevator + in.unit.wash.dryer + in.building.wash.dryer + fitness.center + doorman + pet.policy + near_restaurants_1_5_miles + moderate_restaurants_3_miles, data=apartments_filtered)
 summary(apartments_lm)
 
 plot(apartments_lm)
@@ -55,8 +56,55 @@ apartments_filtered[21247, ]
 apartments_filtered[31297, ]
 apartments_filtered[10647, ]
 
+# re-adding zipcodes back into the apartments_filtered dataset
+# do this by long and lat
+library(sf)
+library(tigris)
+apartments_filtered <- apartments_filtered |> rename(zip = zipcode)
+apartments_filtered <- apartments_filtered |> mutate(zip = as.character(zip))
+
+apartments_full <- apartments_filtered |>
+  left_join(crosswalk_clean, by = "zip") |>
+  left_join(neighborhood_features, by = c("neighborhood_master" = "neighborhood"))
 
 
+# That looks mostly good, thats the apartment features
+# Now we will look at the neighborhood features
+neighborhood_features <- read.csv("master_final.csv")
+
+head(neighborhood_features)
+
+# features: 
+# neighborhood
+# average asking rent
+# market sale price unit
+# inventory units
+# assest value billions
+# vacancy rate
+# median income
+
+# early education enrollment
+# reading test scores 3rd to 8th grade
+# math test scores 3rd to 8th grade
+# graduation rate
+
+# walk score
+# transit score
+# bike score
+# subway station count
+# average routes per station
+# percent ada accessible
+
+# restaurant count
+# cuisine diversity
+# avg restaurant score
+# percent a grade
+# critical violation share
+
+# green space count
+# total green acres
+# average green acres
+# percent waterfront
 
 
 
